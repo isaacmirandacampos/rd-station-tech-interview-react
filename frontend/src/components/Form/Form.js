0a396ab969287ff8e118a2815d23aa66a5b40e0e
Isaac de Miranda Campos <isaacdmcampos@gmail.com>
2024-12-07 12:50:26 -0300

    Initial structure of challenger and add get recommended service

---
+++ frontend/src/components/Form/Form.js
@@ -0,0 +1,54 @@
+// Form.js
+
+import React from 'react';
+import { Preferences, Features, RecommendationType } from './Fields';
+import { SubmitButton } from './SubmitButton';
+import useProducts from '../../hooks/useProducts';
+import useForm from '../../hooks/useForm';
+import useRecommendations from '../../hooks/useRecommendations';
+
+function Form({ applyRecommendations }) {
+  const { preferences, features, products } = useProducts();
+  const { formData, handleChange } = useForm({
+    selectedPreferences: [],
+    selectedFeatures: [],
+    selectedRecommendationType: '',
+  });
+
+  const { getRecommendations } = useRecommendations(products);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    /** @type {Array<Product>} */
+    const dataRecommendations = getRecommendations(formData);
+    applyRecommendations(dataRecommendations)
+  };
+
+  return (
+    <form
+      className="max-w-md mx-auto p-4 bg-white rounded-lg shadow-md"
+      onSubmit={handleSubmit}
+    >
+      <Preferences
+        preferences={preferences}
+        onPreferenceChange={(selected) =>
+          handleChange('selectedPreferences', selected)
+        }
+      />
+      <Features
+        features={features}
+        onFeatureChange={(selected) =>
+          handleChange('selectedFeatures', selected)
+        }
+      />
+      <RecommendationType
+        onRecommendationTypeChange={(selected) =>
+          handleChange('selectedRecommendationType', selected)
+        }
+      />
+      <SubmitButton text="Obter recomendação" />
+    </form>
+  );
+}
+
+export default Form;
---
+++ frontend/src/services/recommendation.service.js
@@ -0,0 +1,77 @@
+// getRecommendations.js
+
+/**
+ * @typedef {Object} Product
+ * @property {number} id - The unique identifier for the product.
+ * @property {string} name - The name of the product.
+ * @property {string} category - The category the product belongs to.
+ * @property {string[]} preferences - The list of preferences related to the product.
+ * @property {string[]} features - The list of features of the product.
+ */
+
+/**
+ * @typedef {Object} FormData
+ * @property {string[] | undefined} selectedPreferences - The list of preferences related to the product.
+ * @property {string[] | undefined} selectedFeatures - The list of features of the product.
+ * @property {'SingleProduct' | 'MultipleProducts'} selectedRecommendationType - the number of recommendations
+ */
+
+/**
+ * @param {FormData} formData - Form data containing selected preferences and features.
+ * @param {Array<Product>} products - List of products to filter and recommend.
+ */
+const getRecommendations = (
+  formData = { selectedPreferences: [], selectedFeatures: [], selectedRecommendationType: 'MultipleProducts' },
+  products
+) => {
+  /** @type {Array<Product>} */
+  const recommendations = []
+  if (formData.selectedRecommendationType === 'MultipleProducts') {
+    for (const product of products) {
+      const score = getScoreOfMatching(product, formData.selectedFeatures, formData.selectedPreferences)
+      if (score > 0) {
+        recommendations.push(product)
+      }
+    }
+  }
+
+  if (formData.selectedRecommendationType === 'SingleProduct') {
+    let filtered = {
+      score: 0,
+      product: null,
+    }
+    for (const product of products) {
+      const score = getScoreOfMatching(product, formData.selectedFeatures, formData.selectedPreferences)
+      if (score > 0 && score >= filtered.score) {
+        filtered = {
+          score,
+          product,
+        }
+      }
+
+    }
+    recommendations.push(filtered.product)
+  }
+
+  return recommendations
+};
+
+/**
+ * @param {Product} product - List of products to filter and recommend.
+ * @param {string[]} features
+ * @param {string[]} preferences
+ * @returns {number}
+ */
+
+const getScoreOfMatching = (product, features= [], preferences= []) => {
+  const featureMatches = product.features.filter(feature => features.includes(feature)).length;
+
+  const preferenceMatches = product.preferences.filter(preference => preferences.includes(preference)).length;
+
+  return featureMatches + preferenceMatches
+}
+
+
+
+
+export default { getRecommendations };
