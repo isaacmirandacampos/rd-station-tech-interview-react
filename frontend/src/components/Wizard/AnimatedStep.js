246c432fc073c154f02b99f2151d03ce4902ac4d
Isaac de Miranda Campos <isaacdmcampos@gmail.com>
2024-12-08 13:11:01 -0300

    remove tailwind variants

--- frontend/src/components/Wizard/AnimatedStep.js
+++ frontend/src/components/Wizard/AnimatedStep.js
@@ -1,24 +1,16 @@
 import React from "react";
-import {tv} from 'tailwind-variants'
-
-const animatedStep = tv({
-  base: 'wizard-step absolute inset-0 transition-all duration-500 ease-in-out opacity-0 transform scale-90 overflow',
-  variants: {
-    state: {
-      actual: 'opacity-100 scale-100 z-10',
-      non_actual: 'opacity-0 scale-90 z-0 pointer-events-none',
-    },
-  },
-});
 
 /**
  * @param {'actual' | 'non_actual'} state - Validate values before submit.
- * @param {jsx} children
+ * @param {Element} children
  */
 
 export const AnimatedStep = ({state, children}) => {
+  const actual = 'opacity-100 scale-100 z-10'
+  const non_actual = 'opacity-0 scale-90 z-0 pointer-events-none'
   return (
-    <div className={animatedStep({state})}>
+    <div
+      className={`wizard-step absolute inset-0 transition-all duration-500 ease-in-out opacity-0 transform scale-90 overflow ${state === 'actual' ? actual : non_actual}`}>
       {children}
     </div>
   )
